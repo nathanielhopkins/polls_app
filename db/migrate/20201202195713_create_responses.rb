7c7cfac307cb5fd9648a6b66515e9177db773aac
“NathanielHopkins” <nathanielchopkins@gmail.com>
2020-12-02 11:59:04 -0800

    CreateResponses migration; write and run

---
+++ db/migrate/20201202195713_create_responses.rb
@@ -0,0 +1,14 @@
+class CreateResponses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :responses do |t|
+      t.integer :user_id
+      t.integer :question_id
+      t.integer :answer_choice_id
+
+      t.timestamps
+    end
+    add_index :responses, :user_id
+    add_index :responses, :question_id
+    add_index :responses, :answer_choice_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_195441) do
+ActiveRecord::Schema.define(version: 2020_12_02_195713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,17 @@ ActiveRecord::Schema.define(version: 2020_12_02_195441) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["poll_id"], name: "index_questions_on_poll_id"
+  end
+
+  create_table "responses", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "question_id"
+    t.integer "answer_choice_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["answer_choice_id"], name: "index_responses_on_answer_choice_id"
+    t.index ["question_id"], name: "index_responses_on_question_id"
+    t.index ["user_id"], name: "index_responses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
