c6b4f016a4d46c3060ddd0d7dd6ce08fe06cfd71
“NathanielHopkins” <nathanielchopkins@gmail.com>
2020-12-02 12:03:19 -0800

    write model User and define has_many :authored_polls and has_many :responses associations

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  has_many(
+    :authored_polls,
+    class_name: 'Poll',
+    foreign_key: :author_id,
+    primary_key: :id
+  )
+
+  has_many(
+    :responses,
+    class_name: 'Response',
+    foreign_key: :user_id,
+    primary_key: :id
+  )
+
+end
