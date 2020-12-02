946ce5a1483340247fb9127ef05571effad080c0
“NathanielHopkins” <nathanielchopkins@gmail.com>
2020-12-02 12:14:37 -0800

    create Response model and write belongs_to :answer_choice and belongs_to :respondent associations

---
+++ app/models/response.rb
@@ -0,0 +1,15 @@
+class Response < ApplicationRecord
+  belongs_to(
+    :answer_choice,
+    class_name: 'AnswerChoice',
+    foreign_key: :answer_choice_id,
+    primary_key: :id
+  )
+
+  belongs_to(
+    :respondent,
+    class_name: 'User',
+    foregin_key: :user_id,
+    primary_key: :id
+  )
+end
