5d6447fef9dca7ef9cc82604bd1f5a047389fb96
“NathanielHopkins” <nathanielchopkins@gmail.com>
2020-12-02 11:54:21 -0800

    CreateQuestions migration; write and run

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_195055) do
+ActiveRecord::Schema.define(version: 2020_12_02_195226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2020_12_02_195055) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_polls_on_author_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text "text"
+    t.integer "poll_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["poll_id"], name: "index_questions_on_poll_id"
   end
 
   create_table "users", force: :cascade do |t|
